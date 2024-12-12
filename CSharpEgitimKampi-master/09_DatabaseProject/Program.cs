9407eb0df5f918894ec5d8ef8ca03b6ef6f991cc
Doruk Duman <77792846+dumandoruk@users.noreply.github.com>
2024-12-12 20:13:34 +0300

    other lessons

--- CSharpEgitimKampi-master/09_DatabaseProject/Program.cs
+++ CSharpEgitimKampi-master/09_DatabaseProject/Program.cs
@@ -13,51 +13,57 @@ namespace _09_DatabaseProject
     {
         static void Main(string[] args)
         {
+
+            #region Sql Listeleme Ornek Arayuz
             //Ado.net
-            Console.WriteLine("***** C# Ürün Kategori Bilgi Sistemi *****");
-            Console.WriteLine();
-            Console.WriteLine();
+            //Console.WriteLine("***** C# Ürün Kategori Bilgi Sistemi *****");
+            //Console.WriteLine();
+            //Console.WriteLine();
 
-            string tableNumber;
-            Console.WriteLine("-----------------------------------");
-            Console.WriteLine("1-Kategoriler");
-            Console.WriteLine("2-Ürünler");
-            Console.WriteLine("3-Siparişler");
-            Console.WriteLine("4-Çıkış Yap");
-            Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
-            tableNumber = Console.ReadLine();
-            Console.WriteLine("-----------------------------------");
-            {
-                string connectionString = ConfigurationManager.ConnectionStrings["DbConnectionString"].ConnectionString;
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
+            //string tableNumber;
+            //Console.WriteLine("-----------------------------------");
+            //Console.WriteLine("1-Kategoriler");
+            //Console.WriteLine("2-Ürünler");
+            //Console.WriteLine("3-Siparişler");
+            //Console.WriteLine("4-Çıkış Yap");
+            //Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+            //tableNumber = Console.ReadLine();
+            //Console.WriteLine("-----------------------------------");
+            #endregion
 
-                    // SqlConnection ile veritabanina baglama islemi
-                    //SqlConnection connection = new SqlConnection("Data Source=yourdatasource;Initial Catalog=DB_EgitimCampi;Integrated Security=True");
-                    connection.Open(); // Baglantiyi ac
+            #region Sql Listeleme Background
+            //{
+            //    string connectionString = ConfigurationManager.ConnectionStrings["DbConnectionString"].ConnectionString;
+            //    using (SqlConnection connection = new SqlConnection(connectionString))
+            //    {
 
-                    // SqlCommand ile baglanilan veritabanina sql sorgusu yazarak istenilen veriyi cekme islemi
-                    SqlCommand command = new SqlCommand("SELECT * FROM Tbl_Categories", connection); // SQL sorgusu
-                    // SqlDataAdapter keywordu ile RAM'de DataTable olusturarak istenilen SQL query sonucunu doldurma islemi
-                    SqlDataAdapter adapter = new SqlDataAdapter(command); // Veriyi almak icin adapter kullan
-                    DataTable dataTable = new DataTable(); // RAM'de bir DataTable olustur
-                    adapter.Fill(dataTable); // Adapter ile DataTable'i doldur
-                    connection.Close(); // Baglantiyi kapat
+            //        // SqlConnection ile veritabanina baglama islemi
+            //        //SqlConnection connection = new SqlConnection("Data Source=yourdatasource;Initial Catalog=DB_EgitimCampi;Integrated Security=True");
+            //        connection.Open(); // Baglantiyi ac
 
-                    // Dongu kullanarak DataTable'daki satirlari gezinme
-                    foreach (DataRow row in dataTable.Rows)
-                    {
-                        // İc ice dongu ile de DataTable'daki sutunlardaki istenilen verileri getirme
-                        foreach (var item in row.ItemArray)
-                        {
-                            Console.WriteLine(item.ToString()); // Her bir ogeyi yazdır
-                        }
-                        Console.WriteLine(); // Satir sonu
-                    }
-                }
+            //        // SqlCommand ile baglanilan veritabanina sql sorgusu yazarak istenilen veriyi cekme islemi
+            //        SqlCommand command = new SqlCommand("SELECT * FROM Tbl_Categories", connection); // SQL sorgusu
+            //        // SqlDataAdapter keywordu ile RAM'de DataTable olusturarak istenilen SQL query sonucunu doldurma islemi
+            //        SqlDataAdapter adapter = new SqlDataAdapter(command); // Veriyi almak icin adapter kullan
+            //        DataTable dataTable = new DataTable(); // RAM'de bir DataTable olustur
+            //        adapter.Fill(dataTable); // Adapter ile DataTable'i doldur
+            //        connection.Close(); // Baglantiyi kapat
 
-                Console.Read();
-            }
+            //        // Dongu kullanarak DataTable'daki satirlari gezinme
+            //        foreach (DataRow row in dataTable.Rows)
+            //        {
+            //            // İc ice dongu ile de DataTable'daki sutunlardaki istenilen verileri getirme
+            //            foreach (var item in row.ItemArray)
+            //            {
+            //                Console.WriteLine(item.ToString()); // Her bir ogeyi yazdır
+            //            }
+            //            Console.WriteLine(); // Satir sonu
+            //        }
+            //    }
+
+            //    Console.Read();
+            //}
+            #endregion
         }
     }
 
---
+++ CSharpEgitimKampi-master/10_DatabaseCrud/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+
+namespace _10_DatabaseCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crud-->Create-Read-Update-Delete
+
+            Console.WriteLine("***** Menu Siparis Islem Paneli *****");
+            Console.WriteLine();
+
+            Console.WriteLine("----------------------------------");
+
+            #region Add Category
+            //Console.WriteLine("Eklemek Istediginiz Kategori Adi: ");
+            //string categoryName = Console.ReadLine();
+            //SqlConnection connection = new SqlConnection("Data Source=DORUKDUMAN;Initial Catalog=DB_EgitimCampi;Integrated Security=True");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("INSERT INTO Tbl_Categories (CategoryName) VALUES (@p1)", connection);
+            //command.Parameters.AddWithValue("@p1", categoryName);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+
+            //Console.WriteLine("Kategori basariyla eklendi!");
+            #endregion
+
+            #region Add Product
+            //string productName;
+            //decimal productPrice;
+            ////bool productStatus;
+            //Console.WriteLine("Add Product: ");
+            //productName = Console.ReadLine();
+            //Console.WriteLine("Product Price: ");
+            //productPrice = decimal.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data Source=DORUKDUMAN; initial catalog=DB_EgitimCampi; integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("INSERT INTO Tbl_Products (ProductName,ProductPrice,ProductStatus) VALUES (@productName,@productPrice,@productStatus)", connection);
+            //command.Parameters.AddWithValue("@productName",productName);
+            //command.Parameters.AddWithValue("@productPrice",productPrice);
+            //command.Parameters.AddWithValue("@productStatus",true);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+
+            //Console.WriteLine("Product Added Successfully!");
+            #endregion
+
+            #region Product List
+
+            //SqlConnection connection = new SqlConnection("Data Source=DORUKDUMAN; initial catalog=DB_EgitimCampi; integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("SELECT * FROM Tbl_Products", connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //DataTable dt = new DataTable();
+            //adapter.Fill(dt);
+            //foreach (DataRow dr in dt.Rows)
+            //{
+            //    foreach (var item in dr.ItemArray)
+            //    {
+            //        Console.Write(item.ToString() + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //connection.Close();
+
+            #endregion
+
+            #region Delete Product
+            //Console.WriteLine("Silinecek Urun Id");
+            //int productId=int.Parse(Console.ReadLine());
+            //SqlConnection connection = new SqlConnection("Data Source=DORUKDUMAN; initial catalog=DB_EgitimCampi; integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("DELETE FROM Tbl_Products Where ProductId=@productId",connection);
+            //command.Parameters.AddWithValue("@productId", productId);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+            //Console.WriteLine("Product Deleted Successfully!");
+            #endregion
+
+            #region Update Product
+            //Console.Write("Guncellenecek Urun Id: ");
+            //int productId = int.Parse(Console.ReadLine());
+
+            //Console.Write("Guncellenecek Urun Ad: ");
+            //string productName = Console.ReadLine();
+
+            //Console.Write("Guncellenecek Urun Fiyati: ");
+            //decimal productPrice = decimal.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data Source=DORUKDUMAN; initial catalog=DB_EgitimCampi; integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("UPDATE Tbl_Products Set ProductName=@productName, ProductPrice=@productPrice where ProductId=@productId", connection);
+            //command.Parameters.AddWithValue("@productName", productName);
+            //command.Parameters.AddWithValue("@productPrice", productPrice);
+            //command.Parameters.AddWithValue("@productId", productId);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+            //Console.Write("Prodcut Updated Successfully!");
+            #endregion
+
+
+            Console.Read();
+        }
+    }
+}
+
